06a4290872d08f959a34d56a904f2b73a630c0be
JoshuaRotimi <62189959+JoshuaRotimi@users.noreply.github.com>
2020-04-11 22:37:14 +0100

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import IndecisionApp from "./component/IndecisionApp";
+import './component/styles/styles.css';
+import './component/styles/base/base.css'
+
+const App = () => {
+    return (
+        <IndecisionApp/>
+    )
+};
+
+
+export default App;
+
